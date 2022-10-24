e7395ade9d95d967d0f770a6fb6d27f4d01123cf
Denise De Jesus <denisedeedejesus@pm.me>
2022-10-24 18:32:38 +0800

    added assets to admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./sass/main.scss";
 
 const Admin = () => {
     return ( 
